685440f383e928799eef512b4aab09ecb2e3f4bc
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-03 20:12:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
+  roughness: 0.5
 });
 
 const mesh = new THREE.Mesh(geometry, material);
